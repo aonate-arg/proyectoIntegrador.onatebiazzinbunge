9049aa653508c5bda5edbbf45307143bf732345b
aonate-arg <aonate@udesa.edu.ar>
2025-11-07 12:52:04 -0300

    index js para productos

--- js/index.js
+++ js/index.js
@@ -1,12 +1,26 @@
 window.addEventListener("load", function(){
-    let url = "https://dummyjson.com/products"
 
-    fecth(url)
+    let productos = document.querySelector("#products")
+
+    let url = 'https://dummyjson.com/products'
+
+    fetch(url)
     .then(function(res){
         return res.json();
     })
     .then(function(data){
-        console.log(data);
+        let resultados = data.products
+        
+        for(let i = 0; i < resultados.length; i++){
+            productos.innerHTML += `
+            <article>
+                <h2>${i[1]}</h2>
+                <h3>${i[3]}</h3>
+                <h4>${i[0]}</h4>
+                <p>${i[2]}</p>
+            </article>
+            `
+        }
     })
     .catch(function(error){
         console.log("error: " + error)
