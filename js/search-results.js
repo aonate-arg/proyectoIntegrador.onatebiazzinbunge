07af49f3830cb59b6293ae9004c6d265dea81c5d
VictorBiazzinRubinho <vbiazzinrubinho@udesa.edu.ar>
2025-11-10 16:10:45 -0300

    un poco de fetch

--- js/search-results.js
+++ js/search-results.js
@@ -6,4 +6,23 @@ window.addEventListener("load", function() {
     let searchTerm = queryStringObj.get('buscador');
     tituloBusqueda.innerHTML = `Resultados de búsqueda para: <strong>${searchTerm}</strong>`;
     let url = `https://dummyjson.com/products/search?q=${searchTerm}`;
+
+    fetch(url)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            let productos = data.products;
+
+            if (productos.length === 0){
+                resultados.innerHTML = "No hay resultados para el término: <strong>${searchTerm}</strong>";
+                return;
+            } 
+            resultados.innerHTML = "";
+            
+            console.log(data);
+        })
+        .catch(function(error){
+            console.log("El error es: " + error);
+        })
 })
