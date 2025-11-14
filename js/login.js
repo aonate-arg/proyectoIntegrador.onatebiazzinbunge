a289eed61d0c78c7f1c47f84c43c3740a5448672
bungem23 <bungem@udesa.edu.ar>
2025-11-14 15:12:06 -0300

    login y saludo

--- js/login.js
+++ js/login.js
@@ -1,12 +1,25 @@
-window.addEventListener("load",function(){
-    let contra = this.document.querySelector("contrasena")
-    let email = this.document.querySelector("email")
-    let logeado = False
+localStorage.clear();
 
-    if(email.length < 0) {
-        this.alert("El campo de email esta vacio")
-    }
-    if (contra.length < 6) {
-        this.alert("La contraseña debe tener minimo 6 caracteres")
-    }
+window.addEventListener("load",function(){
+    let formulario = document.querySelector(".register");
+    let email = this.document.querySelector("#email");
+    let contra = this.document.querySelector("#contrasena");
+    
+
+    localStorage.setItem("emailUsuario", email)
+
+    formulario.addEventListener("submit", function(event){
+        event.preventDefault();
+        if (email.length == 0) {
+            alert("El email esta vacio")
+        }
+
+        if (contra.length == 0) {
+            alert("La contrasena esta vacia")
+        } else if (contra.length < 6) {
+            alert("La contrasena debe tener minimo 6 caracteres!")
+        }
+    })
+
+    localStorage.setItem("emailUsuario", email)
 })
--- js/saludo.js
+++ js/saludo.js
@@ -1,10 +1,4 @@
 window.addEventListener("load",function () {
-    let logeado = True
-    if (logeado) {
-        let email = "this.document.querySelector()"
-        const saludo = this.document.querySelector(".logeado")
-        saludo.innerHTML = "<p>Bienvenido </p> + email"
-    }
-   
-
+    let mail = this.localStorage.getItem("emailUsuario");
+    
 })
