bbc1e88d559f2ff5a6489ddd01c748da262d9a16
VictorBiazzinRubinho <vbiazzinrubinho@udesa.edu.ar>
2025-11-14 12:33:26 -0300

    js search

--- js/search-results.js
+++ js/search-results.js
@@ -1,39 +1,35 @@
 window.addEventListener("load", function() {
     let tituloBusqueda = document.querySelector(".busqueda");
     let resultados = document.querySelector(".resultados");
-    let queryString = location.search;
-    let queryStringObj = new URLSearchParams(queryString);
-    let searchTerm = queryStringObj.get('buscador');
-    tituloBusqueda.innerHTML = `Resultados de búsqueda para: <strong>${searchTerm}</strong>`;
-    let url = `https://dummyjson.com/products/search?q=${searchTerm}`;
-
+    let url = `https://dummyjson.com/products`;
+    
     fetch(url)
         .then(function(response){
             return response.json();
         })
         .then(function(data){
+            let campoBuscar = document.querySelector(".busqueda");
+            if (campoBuscar.value == ""){
+
+            }
+
+            let busqueda = location.search;
+            let busquedaObj = new URLSearchParams(busqueda);
+            let busquedaFinal = busquedaObj.get('busqueda');
+            console.log(busquedaFinal)
 
             resultados.innerHTML = "";
             
             console.log(data);
 
             if (productos.length === 0){
-                resultados.innerHTML = "No hay resultados para el término: <strong>${searchTerm}</strong>";
+                resultados.innerHTML = "No hay resultados para el término: <strong></strong>";
                 return;
             } 
             for(let i = 0; i < resultados.length; i++){
             let producto = resultados[i]
             productos.innerHTML += `
-            <article>
-                <a href="./product.html"> <img class="index" src="${producto.thumbnail}" width="100"> </a>
-                <div>    
-                    <h2>${producto.title}</h2>
-                    <h3>${producto.brand}</h3>
-                    <h4>${producto.category}</h4>
-                    <p>${producto.description}</p>
-                </div>
-                <a id="vermas" href="product.html">Ver +</a>
-            </article>
+                    <h2 class="busqueda">Resultados de la busqueda:${searchTerm}</h2>
             `
             }
         })
