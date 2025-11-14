fb37e214309081c3752db5f7567077ec79fcc631
VictorBiazzinRubinho <vbiazzinrubinho@udesa.edu.ar>
2025-11-14 10:26:24 -0300

    search js

--- js/search-results.js
+++ js/search-results.js
@@ -12,7 +12,6 @@ window.addEventListener("load", function() {
             return response.json();
         })
         .then(function(data){
-            let productos = data.products;
 
             resultados.innerHTML = "";
             
@@ -22,7 +21,21 @@ window.addEventListener("load", function() {
                 resultados.innerHTML = "No hay resultados para el término: <strong>${searchTerm}</strong>";
                 return;
             } 
-            
+            for(let i = 0; i < resultados.length; i++){
+            let producto = resultados[i]
+            productos.innerHTML += `
+            <article>
+                <a href="./product.html"> <img class="index" src="${producto.thumbnail}" width="100"> </a>
+                <div>    
+                    <h2>${producto.title}</h2>
+                    <h3>${producto.brand}</h3>
+                    <h4>${producto.category}</h4>
+                    <p>${producto.description}</p>
+                </div>
+                <a id="vermas" href="product.html">Ver +</a>
+            </article>
+            `
+            }
         })
         .catch(function(error){
             console.log("El error es: " + error);
