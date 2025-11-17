a2552ab2e14978168fe628a22277b542f1884006
bungem23 <bungem@udesa.edu.ar>
2025-11-17 13:48:57 -0300

    logout saludo login

--- js/login.js
+++ js/login.js
@@ -1,25 +1,38 @@
 localStorage.clear();
 
 window.addEventListener("load",function(){
+    let estado = "False"
     let formulario = document.querySelector(".register");
     let email = this.document.querySelector("#email");
     let contra = this.document.querySelector("#contrasena");
+    let alert1 = this.document.querySelector("#alert1");
+    let alert2 = this.document.querySelector("#alert2");
     
-
-    localStorage.setItem("emailUsuario", email)
+   
 
     formulario.addEventListener("submit", function(event){
         event.preventDefault();
-        if (email.length == 0) {
-            alert("El email esta vacio")
+        if (email.value = ""){
+            alert1.innerHTML = "<strong>Este campo es obligatorio</strong>"
+            alert1.style.color = "Red"
         }
 
-        if (contra.length == 0) {
-            alert("La contrasena esta vacia")
+
+
+        if (contra.value == "") {
+            alert2.innerHTML = "<strong>Este campo es obligatorio</strong>"
+            alert2.style.color = "Red"
+
         } else if (contra.length < 6) {
             alert("La contrasena debe tener minimo 6 caracteres!")
+        }
+
+        if (email.length > 0 && contra.length > 6) {
+            estado = "True"
         }
     })
 
     localStorage.setItem("emailUsuario", email)
+    this.localStorage.setItem("estadoLog", estado)
+    
 })
--- js/logout.js
+++ js/logout.js
@@ -1,6 +1,12 @@
 window.addEventListener("load", function () {
-    let caja = document.querySelector(".logeado");
-    caja.addEventListener("click", function(){
+    let estado = this.localStorage.getItem("estadoLog");
+    
+    if(estado == "True"){
+        let caja = document.querySelector(".logeado");
+        caja.addEventListener("click", function(){
         localStorage.clear()
+        let estado = "False"
     })
+    this.localStorage.setItem("estadoLog", estado)
+    }
 })
