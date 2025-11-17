149e7c7f6f0885b6fb8e5568706bb23e0cbd293e
VictorBiazzinRubinho <vbiazzinrubinho@udesa.edu.ar>
2025-11-17 13:50:50 -0300

    search js html

--- js/search-results.js
+++ js/search-results.js
@@ -1,36 +1,50 @@
 window.addEventListener("load", function() {
     let tituloBusqueda = document.querySelector(".busqueda");
     let resultados = document.querySelector(".resultados");
-    let url = `https://dummyjson.com/products`;
+    let queryString = location.search;
+    let queryStringObj = new URLSearchParams(queryString);
+    let searchTerm = queryStringObj.get("busqueda");
+
+    tituloBusqueda.innerHTML = `Resultados de búsqueda para: <strong>${searchTerm}</strong>`;
+
+    let url = `https://dummyjson.com/products/search?q=${searchTerm}`;
     
     fetch(url)
         .then(function(response){
             return response.json();
         })
         .then(function(data){
-            let campoBuscar = document.querySelector(".busqueda");
-            if (campoBuscar.value == ""){
+             let productos = data.products;
 
-            }
+             resultados.innerHTML = "";
 
-            let busqueda = location.search;
-            let busquedaObj = new URLSearchParams(busqueda);
-            let busquedaFinal = busquedaObj.get('busqueda');
-            console.log(busquedaFinal)
-
-            resultados.innerHTML = "";
-            
             console.log(data);
 
             if (productos.length === 0){
-                resultados.innerHTML = "No hay resultados para el término: <strong></strong>";
-                return;
+                resultados.innerHTML = `
+                    No hay resultados para el término: 
+                    <strong>${searchTerm}</strong>
+                `;
             } 
-            for(let i = 0; i < resultados.length; i++){
-            let producto = resultados[i]
-            productos.innerHTML += `
-                    <h2 class="busqueda">Resultados de la busqueda:${searchTerm}</h2>
-            `
+            for(let i = 0; i < productos.length; i++){
+            let producto = productos[i];
+
+                resultados.innerHTML += `
+                    <article>
+                        <a href="./product.html">
+                            <img class="index" src="${producto.thumbnail}" width="100">
+                        </a>
+
+                        <div>
+                            <h2>${producto.title}</h2>
+                            <h3>${producto.brand}</h3>
+                            <h4>${producto.category}</h4>
+                            <p>${producto.description}</p>
+                        </div>
+
+                        <a id="vermas" href="./product.html?id=${producto.id}">Ver más</a>
+                    </article>
+                `;
             }
         })
         .catch(function(error){
