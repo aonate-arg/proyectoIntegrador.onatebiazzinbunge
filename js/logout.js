da8bde2037d5569039151b469679c327c4ad153a
bungem23 <bungem@udesa.edu.ar>
2025-11-19 22:16:08 -0300

    logout terminado

--- js/logout.js
+++ js/logout.js
@@ -5,7 +5,9 @@ window.addEventListener("load", function () {
         let caja = document.querySelector(".logeado");
         caja.addEventListener("click", function(){
             localStorage.clear()
-            let estado = "False"
+            caja.innerHTML = "<li><a href='./login.html'>LOGIN</a></li><li><a href='./register.html'>REGISTRARSE</a></li>";
+            caja.style.listStyleType = "none"
+            caja.style.paddingBottom = "1%"
     })
     this.localStorage.setItem("estadoLog", estado)
     }
