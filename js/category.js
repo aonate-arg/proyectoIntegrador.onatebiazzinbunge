df4c1374683ee856c42200f55548a2c4500d0798
bungem23 <bungem@udesa.edu.ar>
2025-11-21 10:31:28 -0300

    category

---
+++ js/category.js
@@ -0,0 +1,33 @@
+window.addEventListener("load", function(){
+    let url = "https://dummyjson.com/products/categories"
+
+    this.fetch(url)
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(data){
+        console.log(data);
+        let caja = document.querySelector("#categorias")
+        
+        
+        for (let i = 0; i < data.length ; i++) {
+            let categoria = data[i]
+            
+            let nombre = categoria.name
+            caja.innerHTML +=`<a href="./category.html?id=${nombre}"><h3>${nombre}</h3></a>`            
+        }
+
+        let categoria = location.search;
+        let categoriaObj = new URLSearchParams(categoria);
+        let categoriaFin = categoriaObj.get("id")
+        console.log(categoriaFin);
+        
+        
+
+        
+    })
+    .catch(function(error){
+        console.log("el error es: "+ error);
+        
+    })
+})
